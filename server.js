63d9601507e495f5dc71454313889b939d53f621
Alessandro Bulyakhovska Tokaryev <alessandrotokaryev060@gmail.com>
2024-08-15 22:12:36 +0200

    Fixed env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const { default: mongoose } = require('mongoose')
 const APP = express()
 const PORT = 3000;
@@ -13,9 +14,9 @@ APP.use('/api', require('./routes/players'))
 APP.use('/api', require('./routes/game'))
 APP.use('/api', require('./routes/tournament'))
 
-dbUrl = process.env.MONGODBURI
+const uri = process.env.MONGO_URI;
 
-mongoose.connect(dbUrl)
+mongoose.connect(uri)
 .then(APP.listen(PORT, () => console.log(`Connected to database and listening on localhost:${PORT}!`)))
 .catch(err => console.error('Error while connecting.', err))
 
